6dbfc9b6131d810caa065a6cca11775497e4bbba
Kevin Chen <k.chen8880@gmail.com>
2016-02-07 07:08:03 -0500

    Removing unnecessary import statements

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,5 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour {
